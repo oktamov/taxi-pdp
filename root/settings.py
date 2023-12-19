0b5090d6bd78cb7936af734551ef0827595c6efe
oktamov <mr006developer@gmail.com>
2023-12-19 14:43:43 +0500

    initial commit

--- root/settings.py
+++ root/settings.py
@@ -7,9 +7,9 @@ from dotenv import load_dotenv
 load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "django-insecure-%oses6u_d_=9iegtdaaqvy8qy)7%@dto03qylxw*i^_xr*f7rg"
 
-DEBUG = os.getenv('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
